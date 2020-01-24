132c02f37444cfcdc46d3184d33fcc9a131fcc7f
geeksville <kevinh@geeksville.com>
2020-01-23 23:05:43 -0800

    This is a blocking/threaded version of coroutine Continuation

---
+++ src/main/kotlin/com/geeksville/concurrent/SyncContinuation.kt
@@ -0,0 +1,58 @@
+package com.geeksville.concurrent
+
+/**
+ * This is a blocking/threaded version of coroutine Continuation
+ *
+ * A little bit ugly, but the coroutine version has a nasty internal bug that showed up
+ * in my SyncBluetoothDevice so I needed a quick workaround.
+ */
+class SyncContinuation<T> {
+
+    private val mbox = java.lang.Object()
+    private var result: Result<T>? = null
+
+    fun resume(res: T) {
+        synchronized(mbox) {
+            result = Result.success(res)
+            mbox.notify()
+        }
+    }
+
+    fun resumeWithException(ex: Throwable) {
+        synchronized(mbox) {
+            result = Result.failure(ex)
+            mbox.notify()
+        }
+    }
+
+    // Wait for the result (or throw an exception)
+    fun await(): T {  // FIXME, support timeouts
+        synchronized(mbox) {
+            while(result == null) {
+                mbox.wait()
+            }
+
+            val r = result
+            if (r != null)
+                return r.getOrThrow()
+            else
+                throw Exception("This shouldn't happen")
+        }
+    }
+}
+
+/**
+ * Calls an init function which is responsible for saving our continuation so that some
+ * other thread can call resume or resume with exception.
+ *
+ * Essentially this is a blocking version of the (buggy) coroutine suspendCoroutine
+ */
+fun <T> suspend(initfn: (SyncContinuation<T>) -> Unit): T {
+    val cont = SyncContinuation<T>()
+
+    // First call the init funct
+    initfn(cont)
+
+    // Now wait for the continuation to finish
+    return cont.await()
+}
