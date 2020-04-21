a449f224c3a7c76dc20accfa850a1e3555211b66
geeksville <kevinh@geeksville.com>
2020-04-21 11:15:16 -0700

    oops - log messages at error level were not being sent to crashlytics

--- src/main/kotlin/com/geeksville/android/Logging.kt
+++ src/main/kotlin/com/geeksville/android/Logging.kt
@@ -42,7 +42,7 @@ interface Logging {
      * Log an error message, note - we call this errormsg rather than error because error() is
      * a stdlib function in kotlin in the global namespace and we don't want users to accidentally call that.
      */
-    fun errormsg(msg: String, ex: Throwable? = null) = Log.e(tag(), msg, ex)
+    fun errormsg(msg: String, ex: Throwable? = null) = printlog(tag(), msg, ex)
 
     /// Kotlin assertions are disabled on android, so instead we use this assert helper
     fun logAssert(f: Boolean) {
