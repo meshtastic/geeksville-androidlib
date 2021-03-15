6da250358ed13e3c58fd4fa2a123b01b3826d4bf
goga <goga@knopka.com>
2021-03-16 00:05:27 +0500

    display timeAgo properly and show coords in users screen

--- src/main/kotlin/com/geeksville/util/AnyExtensions.kt
+++ src/main/kotlin/com/geeksville/util/AnyExtensions.kt
@@ -2,3 +2,13 @@ package com.geeksville.util
 
 /// A toString that makes sure all newlines are removed (for nice logging).
 fun Any.toOneLineString() = this.toString().replace('\n', ' ')
+
+fun formatAgo(lastSeenUnix: Int): String {
+    val currentTime = (System.currentTimeMillis() / 1000).toInt()
+    val diffMin = (currentTime - lastSeenUnix) / 60;
+    if (diffMin < 100)
+        return diffMin.toString() + "m"
+    if (diffMin < 6000)
+        return (diffMin / 60).toString() + "h"
+    return (diffMin / (60 * 24)).toString() + "d";
+}
